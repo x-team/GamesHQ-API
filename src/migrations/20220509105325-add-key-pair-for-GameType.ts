dee62f402c1a2dae4b74641fc3daff5dc53fd5ac
Cristian Morales <ccmj202@gmail.com>
2022-05-09 12:08:12 -0500

    🚀 XTG-191: DB Re-Structure for GameType to handle API calls

---
+++ src/migrations/20220509105325-add-key-pair-for-GameType.ts
@@ -0,0 +1,93 @@
+import { DataTypes, QueryInterface, QueryTypes, Sequelize, Transaction } from 'sequelize';
+import { generateSecret } from '../utils/cryptography';
+
+interface SequelizeContext {
+  context: {
+    queryInterface: QueryInterface;
+    Sequelize: Sequelize;
+  };
+}
+
+interface LightEntity {
+  id: number;
+}
+
+const firstUserEmail = 'cristian.cmj@x-team.com';
+
+function queryByProp(
+  table: string,
+  prop: string,
+  value: string,
+  queryInterface: QueryInterface,
+  transaction: Transaction
+): Promise<LightEntity[]> {
+  const queryString = `SELECT id, "${prop}" FROM "${table}" WHERE "${prop}" = '${value}';`;
+  return queryInterface.sequelize.query(queryString, {
+    transaction,
+    type: QueryTypes.SELECT,
+  });
+}
+
+module.exports = {
+  async up({ context: { queryInterface } }: SequelizeContext) {
+    return queryInterface.sequelize.transaction(async (transaction) => {
+      await queryInterface.addColumn(
+        'GameType',
+        'clientSecret',
+        {
+          type: DataTypes.TEXT,
+          allowNull: false,
+          defaultValue: await generateSecret(),
+        },
+        {
+          transaction,
+        }
+      );
+      await queryInterface.addColumn(
+        'GameType',
+        'signingSecret',
+        {
+          type: DataTypes.TEXT,
+          allowNull: false,
+          defaultValue: await generateSecret(),
+        },
+        {
+          transaction,
+        }
+      );
+
+      const [firstUser] = (await queryByProp(
+        'User',
+        'email',
+        firstUserEmail,
+        queryInterface,
+        transaction
+      )) as LightEntity[];
+
+      await queryInterface.addColumn(
+        'GameType',
+        '_createdBy',
+        {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          defaultValue: firstUser.id,
+          references: {
+            model: 'User',
+            key: 'id',
+          },
+        },
+        {
+          transaction,
+        }
+      );
+    });
+  },
+
+  async down({ context: { queryInterface } }: SequelizeContext) {
+    return queryInterface.sequelize.transaction(async (transaction) => {
+      await queryInterface.removeColumn('GameType', 'clientSecret', { transaction });
+      await queryInterface.removeColumn('GameType', 'signingSecret', { transaction });
+      await queryInterface.removeColumn('GameType', '_createdBy', { transaction });
+    });
+  },
+};
--- src/models/GameType.ts
+++ src/models/GameType.ts
@@ -1,6 +1,16 @@
-import { Table, Column, Model, DataType, PrimaryKey } from 'sequelize-typescript';
+import { Association } from 'sequelize';
+import {
+  Table,
+  Column,
+  Model,
+  DataType,
+  PrimaryKey,
+  BelongsTo,
+  AllowNull,
+} from 'sequelize-typescript';
 
 import { GAME_TYPE } from '../games/consts/global';
+import { User } from './';
 
 interface GameTypeAttributes {
   id: GAME_TYPE;
@@ -18,4 +28,23 @@ export class GameType
   @PrimaryKey
   @Column(DataType.TEXT)
   id!: GAME_TYPE;
+
+  @AllowNull(false)
+  @Column(DataType.TEXT)
+  clientSecret!: string;
+
+  @AllowNull(false)
+  @Column(DataType.TEXT)
+  signingSecret!: string;
+
+  @BelongsTo(() => User, {
+    foreignKey: '_createdById',
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+  })
+  _createdBy?: User;
+
+  static associations: {
+    _createdBy: Association<GameType, User>;
+  };
 }
