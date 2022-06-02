dfcf85a3d159a9526a1aae329205ba79f1a4eacc
Cristian Morales <ccmj202@gmail.com>
2022-06-01 19:13:30 -0500

    🐞 HotFix: Remove alternative until we find a better validator

--- src/api-utils/responseSchemas/user.ts
+++ src/api-utils/responseSchemas/user.ts
@@ -17,23 +17,19 @@ const userSessionSchema = Joi.object({
   profilePictureUrl: Joi.string().optional(),
   role: Joi.number().required(),
   isAdmin: Joi.boolean().required(),
-})
-  .required()
-  .options({ stripUnknown: true });
+}).options({ stripUnknown: true });
 
 const sessionSchema = Joi.object({
   success: sessionStateSchema,
   session: sessionTokenSchema,
   user: userSessionSchema,
-})
-  .required()
-  .options({ stripUnknown: true });
+}).options({ stripUnknown: true });
 
 const sessionGenericSchema = Joi.object({
   success: sessionStateSchema,
   message: sessionErrorMessageSchema,
 }).required();
 
-export const checkAvailableSessionSchema = Joi.alternatives([sessionSchema, sessionGenericSchema]);
+export const checkAvailableSessionSchema = sessionSchema;
 
 export const logoutSessionSchema = sessionGenericSchema;
